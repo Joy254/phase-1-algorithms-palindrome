2a7779923b7c644f18a12691bb53608a96a9d730
JOY254 <nyorgijoy@gmail.com>
2023-10-05 00:51:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+// function isPalindrome(word) {
+//   let leftpointer = 0;
+//   let rightpointer = word.length -1;
+//   let ans = false
 
+//   while(leftpointer < rightpointer) {
+//     if(word[leftpointer] === word[rightpointer]){ 
+//       ans =true
+//       leftpointer += 1;
+//       rightpointer-= 1;
+//     }
+//     else{
+//       ans=false
+//       return
+//     }
+//   }
+//   return ans
+// }
+// console.log(isPalindrome ("abba")),
+// console.log(isPalindrome("racecar")),
+// console.log(isPalindrome("a")),
+// console.log(isPalindrome("robot")),
+// console.log(isPalindrome("ab"))
+
+  // Write your algorithm here
+//   let reversed=word.split("").reversed().join("")
+//   console.log(reversed)
+//   if(reversed === word){
+//     return true;
+//   }
+//   else{
+//     return false;
+//   }
+// }
+// console.log isPalindrome("abba")
 /* 
   Add your pseudocode here
 */
@@ -14,12 +45,18 @@ function isPalindrome(word) {
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
 }
 
 module.exports = isPalindrome;
